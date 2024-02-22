2a81374628b1803ff14232b1d2901801785f5da9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-02-22 13:47:24 -0500

    feat: Add the filesorter/filesorter/approach.py.

---
+++ filesorter/filesorter/approach.py
@@ -0,0 +1,15 @@
+"""Configuration of the benchmarking tool with an approach."""
+
+from enum import Enum
+
+
+class SortApproach(str, Enum):
+    """Define the name for the approach for sorting a list contain person objects."""
+
+    lambdafunction = "lambdafunction"
+    attrgetter = "attrgetter"
+    customcompare = "customcompare"
+
+    def __str__(self):
+        """Define a default string representation."""
+        return self.value
