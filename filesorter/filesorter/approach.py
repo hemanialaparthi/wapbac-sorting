31e68dffcb552e27da4f24795aaef15c338eacbd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-02-25 09:10:36 -0500

    chore: Add more examples of the approaches to the filesorter/filesorter/approach.py file.

--- filesorter/filesorter/approach.py
+++ filesorter/filesorter/approach.py
@@ -4,9 +4,11 @@ from enum import Enum
 class SortApproach(str, Enum):
     """Define the name for the approach for sorting a list contain person objects."""
 
-    lambdafunction = "lambdafunction"
     attrgetter = "attrgetter"
+    bubblesort = "bubblesort"
     customcompare = "customcompare"
+    lambdafunction = "lambdafunction"
+    quicksort = "quicksort"
 
     def __str__(self):
         """Define a default string representation."""
