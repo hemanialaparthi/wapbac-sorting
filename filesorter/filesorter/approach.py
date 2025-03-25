09f7d89ebb638f59dd7b05eb317fc6cfef16c324
Grant Anderson <143619033+ganderson03@users.noreply.github.com>
2025-03-24 22:46:04 -0400

    Update approach.py

--- filesorter/filesorter/approach.py
+++ filesorter/filesorter/approach.py
@@ -2,17 +2,15 @@ from enum import Enum
 
 
 class SortApproach(str, Enum):
-    """Define the cool name for the approach for sorting a list contain person objects."""
+    """Define the cool name for the approach for sorting a list contain person objects"""
 
     attrgetter = "attrgetter"
-    bubblesort = "bubblesort"
-    customcompare = "customcompare"
-    lambdafunction = "lambdafunction"
+    timsort = "timsort"
     quicksort = "quicksort"
     bubblesort_multilevel = "bubblesort_multilevel"
     timsort_multilevel = "timsort_multilevel"
     quicksort_multilevel = "quicksort_multilevel"
 
     def __str__(self):
-        """Define a default string representation."""
+        """Define a default string representation"""
         return self.value
